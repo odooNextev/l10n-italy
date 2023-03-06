88ed5d7cef5522f99a01646a2d2b3493e5c3e1de
stenex <stefano.nana@nextev.it>
2023-03-06 16:44:56 +0100

    [IMP] Aggiunto commenti

--- l10n_it_sale_commission_riba/__manifest__.py
+++ l10n_it_sale_commission_riba/__manifest__.py
@@ -1,4 +1,3 @@
-# Copyright 2018 Tecnativa - Sergio Teruel
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     'name': 'Sales commissions Riba',
--- l10n_it_sale_commission_riba/wizard/wizard_riba_commissions_settle.py
+++ l10n_it_sale_commission_riba/wizard/wizard_riba_commissions_settle.py
@@ -1,5 +1,7 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
 from datetime import date, timedelta
-from odoo import models, fields, api, _
+from odoo import models, api, _
 
 
 class SaleCommissionMakeSettle(models.TransientModel):
@@ -7,30 +9,41 @@ class SaleCommissionMakeSettle(models.TransientModel):
 
     @api.multi
     def action_settle(self):
+        """
+        Esclude dal calcolo delle commissioni le fatture insolute, quelle con termini di
+        pagamento Ri.Ba salvo buon fine se non sono passati almeno 5 giorni dalla data di 
+        scadenza e quelle che hanno impostato manualmente il flag 'no_commission' 
+        (per esempio se ormai è insoluta da anni).
+        Il metodo predefinito non prevede hooks quindi è necessario riscriverlo completamente.
+        """
         self.ensure_one()
         agent_line_obj = self.env['account.invoice.line.agent']
         settlement_obj = self.env['sale.commission.settlement']
         settlement_line_obj = self.env['sale.commission.settlement.line']
         settlement_ids = []
         if not self.agents:
-            self.agents = self.env['res.partner'].search(
-                [('agent', '=', True)])
+            self.agents = self.env['res.partner'].search([('agent', '=', True)])
         date_to = self.date_to
         for agent in self.agents:
             date_to_agent = self._get_period_start(agent, date_to)
             # Get non settled invoices
-            agent_lines = agent_line_obj.search(
-                [('invoice_date', '<=', date_to),
-                ('agent', '=', agent.id),
-                ('settled', '=', False)], order='invoice_date')
+            agent_lines = agent_line_obj.search([('invoice_date', '<=', date_to_agent),
+                                                 ('agent', '=', agent.id), ('settled', '=', False)],
+                                                order='invoice_date')
+            # inizio modifica per Ri.Ba
             for line in agent_lines:
+                # rimuove le righe delle fatture che hanno impostato il flag "no_commission"
                 if line.invoice.no_commission:
                     agent_lines = agent_lines - line
+                # filtro su Ri.Ba
                 elif line.invoice.payment_term_id.riba:
-                    # XXX: se sono passati almeno 5 giorni dalla data di scadenza considero la fattura pagata
-                    # altrimenti rimuovo le commmissioni
-                    if line.invoice.is_unsolved or line.invoice.date_due + timedelta(days=+5) > date.today():
+                    # rimuove le righe se la ri.ba è insoluta o nel caso sia sbf non siano
+                    # passati almeno 5 giorni dalla data di scadenza del pagamento per
+                    # tenersi un margine e verificare che effettivamente sia stata pagata
+                    if line.invoice.is_unsolved or line.invoice.date_due + timedelta(
+                            days=+5) > date.today():
                         agent_lines = agent_lines - line
+            # fine modifica per Ri.Ba
             for company in agent_lines.mapped('company_id'):
                 agent_lines_company = agent_lines.filtered(
                     lambda r: r.object_id.company_id == company)
@@ -44,31 +57,15 @@ class SaleCommissionMakeSettle(models.TransientModel):
                     if line._skip_settlement():
                         continue
                     if line.invoice_date > sett_to:
-                        # per compatibilità vecchio sistema
-                        sett_from = self._get_period_start(
-                            agent, line.invoice_date)
+                        sett_from = self._get_period_start(agent, line.invoice_date)
                         sett_to = self._get_next_period_date(
-                            agent, sett_from,
+                            agent,
+                            sett_from,
                         ) - timedelta(days=1)
-                        settlement1 = self._get_settlement(
-                            agent, company, sett_from, sett_to)
-                        # nuovo sistema
-                        sett_from = date_to_agent
-                        sett_to = self._get_next_period_date(
-                            agent, sett_from,
-                        ) - timedelta(days=1)
-                        settlement2 = self._get_settlement(
-                            agent, company, sett_from, sett_to)
-
-                        if not settlement1:
-                            if settlement2:
-                                settlement=settlement2
-                            else:
-                                settlement = settlement_obj.create(
-                                    self._prepare_settlement_vals(
-                                        agent, company, sett_from, sett_to))
-                        else:
-                            settlement = settlement1
+                        settlement = self._get_settlement(agent, company, sett_from, sett_to)
+                        if not settlement:
+                            settlement = settlement_obj.create(
+                                self._prepare_settlement_vals(agent, company, sett_from, sett_to))
                         settlement_ids.append(settlement.id)
                     settlement_line_obj.create({
                         'settlement': settlement.id,
