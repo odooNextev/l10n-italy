53a919a0440387be0ba936e79489f27ef01392a8
matteo.tognini <matteo.tognini@nextev.it>
2023-06-13 16:54:57 +0200

    [IMP]Contatore per ddt portal

--- l10n_it_delivery_note_rma/controllers/portal.py
+++ l10n_it_delivery_note_rma/controllers/portal.py
@@ -1,2 +1,66 @@
-#  Copyright 2019 Simone Rubino - Agile Business Group
-#  License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import binascii
+
+from odoo import fields, http, SUPERUSER_ID, _
+from odoo.exceptions import AccessError, MissingError
+from odoo.http import request
+from odoo.addons.payment.controllers.portal import PaymentProcessing
+from odoo.addons.portal.controllers.mail import _message_post_helper
+from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager, get_records_pager
+from odoo.osv import expression
+
+
+class DDTCustomerPortal(CustomerPortal):
+
+    def _prepare_home_portal_values(self, counters):
+        values = super()._prepare_home_portal_values(counters)
+        ddt_count = self.env['stock.delivery.note'].search_count([('partner_id', '=', self.user.id)])
+        values.update({
+            'ddt_count' : 5
+        })
+        return values
+
+#    @http.route(['/my/delivery-notes', '/my/delivery-notes/page/<int:page>'], type='http', auth="user", website=True)
+#     def portal_my_quotes(self, page=1, date_begin=None, date_end=None, sortby=None, **kw):
+#         values = self._prepare_portal_layout_values()
+#         partner = request.env.user.partner_id
+#         SaleOrder = request.env['sale.order']
+
+        domain = self._prepare_quotations_domain(partner)
+
+        searchbar_sortings = self._get_sale_searchbar_sortings()
+
+        # default sortby order
+        if not sortby:
+            sortby = 'date'
+        sort_order = searchbar_sortings[sortby]['order']
+
+        if date_begin and date_end:
+            domain += [('create_date', '>', date_begin), ('create_date', '<=', date_end)]
+
+        # count for pager
+        quotation_count = SaleOrder.search_count(domain)
+        # make pager
+        pager = portal_pager(
+            url="/my/quotes",
+            url_args={'date_begin': date_begin, 'date_end': date_end, 'sortby': sortby},
+            total=quotation_count,
+            page=page,
+            step=self._items_per_page
+        )
+        # search the count to display, according to the pager data
+        quotations = SaleOrder.search(domain, order=sort_order, limit=self._items_per_page, offset=pager['offset'])
+        request.session['my_quotations_history'] = quotations.ids[:100]
+
+        values.update({
+            'date': date_begin,
+            'quotations': quotations.sudo(),
+            'page_name': 'quote',
+            'pager': pager,
+            'default_url': '/my/quotes',
+            'searchbar_sortings': searchbar_sortings,
+            'sortby': sortby,
+        })
+        return request.render("sale.portal_my_quotations", values)
