51fec635415191763a3f1cb41dc0862c4b15334b
matteo.tognini <matteo.tognini@nextev.it>
2023-06-13 16:28:06 +0200

    [IMP]Aggiunto template ddt portale

--- l10n_it_delivery_note_rma/__manifest__.py
+++ l10n_it_delivery_note_rma/__manifest__.py
@@ -1,8 +1,8 @@
 #  Copyright 2019 Simone Rubino - Agile Business Group
 #  License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "ITA - Codice IPA - Portale",
-    "summary": "Aggiunge l'indice PA (IPA) tra i dettagli dell'utente nel portale.",
+    "name": "ITA - DDT - Portale",
+    "summary": "Aggiunge i DDT nel portale.",
     "version": "14.0.1.0.0",
     "category": "Localization/Italy",
     "website": "https://github.com/OCA/l10n-italy",
@@ -15,5 +15,4 @@
     "data": [
         "views/portal_templates.xml",
     ],
-    "auto_install": True,
 }
