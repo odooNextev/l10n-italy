3110b8c6e365f7102bb70a8b29439008fd200ce0
odooNextev <odoo@nextev.it>
2025-03-26 09:07:17 +0100

    [ADD] l10n_it_edi_extension

---
+++ l10n_it_edi_extension/__manifest__.py
@@ -0,0 +1,28 @@
+# Copyright 2025 Giuseppe Borruso - Dinamiche Aziendali srl
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Italy - E-invoicing - Base Feature",
+    "version": "18.0.1.0.0",
+    "category": "Accounting/Localizations/EDI",
+    "development_status": "Production/Stable",
+    "summary": "E-invoice base feature",
+    "author": "Giuseppe Borruso, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-italy",
+    "license": "AGPL-3",
+    "depends": [
+        "partner_firstname",
+        "account",
+        "l10n_it_edi",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/invoice_it_template.xml",
+        "views/l10n_it_view.xml",
+        "views/res_partner_view.xml",
+        "wizards/l10n_it_edi_import_file_wizard.xml",
+    ],
+    "installable": True,
+    "pre_init_hook": "_l10n_it_edi_extension_pre_init_hook",
+    "post_init_hook": "_l10n_it_edi_extension_post_init_hook",
+}
