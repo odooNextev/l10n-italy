949e66a6fe3afba81711ca298ec77ca94d48130a
Marco Colombo <marco.colombo@gmail.com>
2025-04-04 20:46:02 +0200

    [IMP] l10n_it_vat_settlement_date: add XLSX report

--- l10n_it_vat_settlement_date/wizard/__init__.py
+++ l10n_it_vat_settlement_date/wizard/__init__.py
@@ -1,3 +1,3 @@
-# Copyright (c) 2021 Marco Colombo <https://github/TheMule71>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
 from . import report_registro_iva_xlsx
